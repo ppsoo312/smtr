b63db0ca7e86c979cb5dcbc9cb1a10d384bc6a55
YounSooPark <YounSooPark@pys>
2023-03-13 18:00:54 +0900

    2023-03-13_1

---
+++ SMTR/src/main/java/com/sy/interceptor/LoginInterceptor.java
@@ -0,0 +1,48 @@
+package com.sy.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+
+
+public class LoginInterceptor implements HandlerInterceptor{
+	
+	private static final Logger logger = LoggerFactory.getLogger(LoginInterceptor.class);
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		
+		boolean result = false;
+		String url = request.getRequestURI();
+		
+		if(request.getSession().getAttribute("login") == null) {
+			
+			if(url.equals("/smtr/addRecipe")) {
+				return false;
+			} else {
+				return true;
+			}
+		}
+		return true;
+	}
+	
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
